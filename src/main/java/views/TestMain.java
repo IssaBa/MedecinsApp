6a2a7bc166818c533a8aea08b55fdec2434ffa10
Cheikh Abdou Lahad Diagne <princenaar@gmail.com>
2020-05-18 23:36:24 +0000

    cald

---
+++ src/main/java/views/TestMain.java
@@ -0,0 +1,28 @@
+package views;
+
+import dao.MedecinUserDAO;
+import models.MedecinUser;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		
+		MedecinUserDAO usDao = new MedecinUserDAO();
+        MedecinUser users1 = new MedecinUser();
+        users1.setMatricule("I2002");
+        users1.setNom("BA");
+        users1.setPrenom("Issa");
+        users1.setUsername("Isac");
+        users1.setPassword("test");
+        
+        
+      // save users
+        usDao.saveUser(users1);
+        
+      // find users
+        System.out.println(usDao.searchUser(users1).getMatricule());
+      
+	}
+
+}
